f5caca466df5543833b8d8dd7bb36b8f7e86dd91
Amitree <amitree@Amitrees-Mac-mini.local>
2014-02-13 11:58:44 -0800

    [#61302854] adding install script, updating README.md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 heroku-auto-deploy
 ==================
 
-Auto deployment to production when stories are accepted
+Automatically deploy code from staging to production on Heroku when stories are
+accepted in Pivotal Tracker.
+
+Usage
+-----
+
+1. `git clone https://github.com/amitree/heroku-auto-deploy.git`
+1. Copy `lib/secrets.rb.template` to `lib/secrets.rb` and fill in the necessary information.
+1. On Mac OS X, run `./install.sh`.  This will set up a launchd process that runs every 15 minutes, with output being logged to `/var/log/deploy_to_heroku.log`.
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+function cleanup()
+{
+	rm -f bin/deploy_to_heroku.tmp
+}
+
+trap cleanup EXIT
+
+dir=$(cd $(dirname "$0"); pwd)
+sudo touch /var/log/deploy_to_heroku.log
+sudo chmod 0666 /var/log/deploy_to_heroku.log
+sudo mkdir -p /usr/local/bin
+sudo sed -e "s,__DIR__,${dir},g" <bin/deploy_to_heroku_wrapper >bin/deploy_to_heroku.tmp
+chmod 0755 bin/deploy_to_heroku.tmp
+sudo cp bin/deploy_to_heroku.tmp /usr/local/bin/deploy_to_heroku
+sudo chmod 0755 /usr/local/bin/deploy_to_heroku
+
+sudo cp launchd/com.amitree.herokuAutoDeploy.plist /Library/LaunchDaemons
+launchctl load -w /Library/LaunchDaemons/com.amitree.herokuAutoDeploy.plist
